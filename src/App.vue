9f26b6df1c008fbd6b5743c5e5dee5cfbc5a78fd
VitaliiHoida <vitaliy.gojda@gmail.com>
2022-12-09 14:13:30 +0200

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,78 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="wrapper">
+    <div class="container">
+      <div class="logo">
+        <img src="@/assets/logo.png" alt="logo">
+      </div>
+      <div class="content">
+        <button type="button" class="active">Главная</button>
+        <button type="button">Избранное</button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: {}
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+* {
+  margin: 0;
+  padding: 0;
+}
+
+.wrapper {
+  width: 100%;
+  max-width: 1200px;
+  min-width: 360px;
+  margin: 0 auto;
+}
+
+.container {
+  box-sizing: border-box;
+  padding: 20px 10px 0 10px;
+  width: 100%;
+}
+
+.logo {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin-bottom: 40px;
+}
+
+.logo img {
+  max-height: 100px;
+  object-fit: contain;
+}
+
+.content button {
+  padding: 10px 15px;
+  font-size: 16px;
+  line-height: 1;
+  font-weight: 500;
+  cursor: pointer;
+  text-transform: uppercase;
+  color: #fff;
+  background: #1a73e8;
+  border: none;
+  border-radius: 5px;
+  transition: 0.3s ease-in;
+}
+
+.content button:hover,
+.content button:active {
+  background: #4285f4;
+  transition: 0.3s ease-in;
+}
+
+.content button.active {
+  border: 1px solid black;
+}
+
+.content button:first-child {
+  margin-right: 20px;
 }
 </style>
